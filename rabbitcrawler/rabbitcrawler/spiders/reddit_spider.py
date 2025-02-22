107a8c2dcdafdb933f95bc37d01f0016d8ba1918
Jayden Lee <jaydenlee.swe@gmail.com>
2025-02-22 16:37:50 -0500

    fix spider 2

--- rabbitcrawler/rabbitcrawler/spiders/reddit_spider.py
+++ rabbitcrawler/rabbitcrawler/spiders/reddit_spider.py
@@ -1,37 +1,22 @@
 import scrapy
-
 from ..items import Reddit
 
-class RedditSpider(scrapy.Spider):
+class RedditCrawler(scrapy.Spider):
     name = "reddit"
-
-    def __init__(self, query):
-        self.query = query
-        self.start_urls = ["https://www.reddit.com/"]
-
-    def start_requests(self):
-        base_url = f"https://www.reddit.com/search?q={self.query}"
-        urls = [
-            base_url + self.query,
-        ]
-        for url in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
-
+    
+    def __init__(self, query, *args, **kwargs):
+        super(RedditCrawler, self).__init__(*args, **kwargs)
+        self.start_urls = [f"https://www.reddit.com/search/?q={query}"]
+        
     def parse(self, response):
         post_links = response.css("a[data-testid='post-title']::attr(href)").getall()[:5]
         for post_link in post_links:
-            post = Reddit()
+            post=Reddit()
             full_url = response.urljoin(post_link)
-            yield scrapy.Request(url=full_url, callback=self.parse_post, meta={"post": post}) 
-
+            yield scrapy.Request(url=full_url, callback=self.parse_post, meta={"post": post})
+            
     def parse_post(self, response):
         post = response.meta["post"]
         post["url"] = response.url
         post["title"] = response.css("h1::text").get().strip()
         yield post
-
-        # next_page_url = response.css("li.next>a::attr(href)").extract_first()
-
-        # if next_page_url:
-        #     next_page_url = response.urljoin(next_page_url)
-        #     yield scrapy.Request(url=next_page_url, callback=self.parse)
